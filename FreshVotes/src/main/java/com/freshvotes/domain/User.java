fe6ace9163fffdaf725f92a4903afc83982cd0ba
Oladejo Olajide <programmerolajide@gmail.com>
2020-01-09 23:07:03 +0100

    First commit

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/User.java
@@ -0,0 +1,13 @@
+package com.freshvotes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	private Long id;
+	
+
+}
