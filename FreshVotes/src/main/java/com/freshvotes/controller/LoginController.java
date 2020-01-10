25fc5fd87090fa4b4d0ac739f806c97654be6f05
Oladejo Olajide <programmerolajide@gmail.com>
2020-01-10 16:44:42 +0100

    Login controller created

--- FreshVotes/src/main/java/com/freshvotes/controller/LoginController.java
+++ FreshVotes/src/main/java/com/freshvotes/controller/LoginController.java
@@ -1,14 +1,15 @@
 package com.freshvotes.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
+public class LoginController {
 	
-	@GetMapping("/")
-	public String rootView() {
+	@GetMapping("/login")
+	public String login() {
 		
-		return "index";
+		return "login";
 	}
 
 }
